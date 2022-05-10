e7b87dad3fe15d3cd88a87a2bc0c613c365b4e7f
Patrik Hermansson <hermansson.patrik@gmail.com>
2022-05-10 20:57:07 +0200

    Add url

--- lib/lib1/libfile1.c
+++ lib/lib1/libfile1.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int libfile1()
 {
-    printf("libfile1");
+    printf("libfile1\n");
     return 0;
 }
--- src/main.c
+++ src/main.c
@@ -1,9 +1,25 @@
+/**
+ * @file main.c
+ * @author Patrik Hermansson (hermansson.patrik@gmail.com)
+ * @brief 
+ * 
+ * gcc src/main.c lib/lib1/libfile1.c -o main -Ilib/lib1
+ * 
+ * https://www.cs.colby.edu/maxwell/courses/tutorials/maketutor/
+ * 
+ * @version 0.1
+ * @date 2022-05-10
+ * 
+ * @copyright (c) Patrik Hermansson 2022
+ * 
+ */
+
 #include <stdio.h>
-#include <libfile1>
+#include <libfile1.h>
 
 int main()
 {
-    printf("main");
+    printf("In main\n");
     libfile1();
     return 0;
 }
